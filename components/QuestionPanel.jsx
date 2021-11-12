d911f6694f07d562614bfdd82ba538afa578434f
rxrosli <raymark.roslin@gmail.com>
2021-11-12 11:06:57 +0800

    fix: change option property access from content to value

--- components/QuestionPanel.jsx
+++ components/QuestionPanel.jsx
@@ -24,7 +24,7 @@ const OptionField = ({ option, onToggleClick, onValueBlur }) => {
       <TextareaAutosize
         onBlur={onValueBlur}
         placeholder="Type your option"
-        value={option.content}
+        value={option.value}
       ></TextareaAutosize>
     </fieldset>
   );
