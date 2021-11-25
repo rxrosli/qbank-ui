e5672c882b7f32ab343fa77eeb7c4cd649c3bd93
rxrosli <raymark.roslin@gmail.com>
2021-11-25 09:21:21 +0800

    refactor: removed project custom settings
    refactor: update readme to reflect as typescript project
    
    refactor: remove dummy data, create props for QuestionPanel

--- components/QuestionPanel.tsx
+++ components/QuestionPanel.tsx
@@ -1,36 +1,14 @@
 import React, { useState } from "react";
-import TextareaAutosize from "react-textarea-autosize";
 import IQuestion from "../models/IQuestion";
 import IOption from "../models/IOptions";
+import TextareaAutosize from "react-textarea-autosize";
 import OptionField from "./OptionField";
 
-const data: IQuestion = {
-	uuid: "5f2ed776-24a8-423b-b0b1-2401d1944cb0",
-	type: "Multiple Choice",
-	stem: "The adrenal medulla secretes which of the following in the greatest quantity?",
-	options: [
-		{
-			isToggled: false,
-			value: "Hello World"
-		},
-		{
-			isToggled: false,
-			value: "Hello World"
-		},
-		{
-			isToggled: false,
-			value: "Hello"
-		},
-		{
-			isToggled: false,
-			value: "Hello"
-		}
-	],
-	tags: ["Parasitology", "Chemistry", "Biology"]
+type Props = {
+	question: IQuestion;
 };
-
-const QuestionPanel = () => {
-	const [question, setQuestion] = useState<IQuestion>(data);
+const QuestionPanel = (props: Props) => {
+	const [question, setQuestion] = useState<IQuestion>(props.question);
 
 	function toggleOption(option: IOption) {
 		setQuestion({
@@ -55,9 +33,6 @@ const QuestionPanel = () => {
 		});
 	}
 	function deleteOption(option: IOption) {
-		// setOptions(options => {
-		// 	return options.filter(previousOptions => previousOptions !== option);
-		// });
 		setQuestion({
 			...question,
 			options: question.options.filter(prevOpt => prevOpt !== option)
