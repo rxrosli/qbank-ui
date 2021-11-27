2af828bc98992dddb3f313dccaf06506bc5b81cd
rxrosli <raymark.roslin@gmail.com>
2021-11-27 23:47:25 +0800

    feat: add dummy data yo question view page, implement Article Container in page

--- pages/questions/view.tsx
+++ pages/questions/view.tsx
@@ -1,9 +1,42 @@
 import Head from "next/head";
 import Header from "../../components/Header";
 import Navigation from "../../components/Navigation";
-import QuestionArticle from "../../components/QuestionArticle";
+import ArticleContainer from "../../container/ArticleContainer";
+import IQuestion from "../../models/IQuestion";
 import React from "react";
 
+const question: IQuestion = {
+	uuid: "5f2ed776-24a8-423b-b0b1-2401d1944cb0",
+	type: "Multiple Choice",
+	stem: "The adrenal medulla secretes which of the following in the greatest quantity?",
+	options: [
+		{
+			isToggled: true,
+			value: "Hello World"
+		},
+		{
+			isToggled: true,
+			value: "Hello World"
+		},
+		{
+			isToggled: false,
+			value: "Hello"
+		},
+		{
+			isToggled: false,
+			value: "Hello"
+		}
+	],
+	tags: ["Parasitology", "Chemistry", "Biology"]
+};
+
+const questionSet: IQuestion[] = [
+	{ ...question },
+	{ ...question },
+	{ ...question },
+	{ ...question },
+	{ ...question }
+];
 function view() {
 	return (
 		<div>
@@ -13,13 +46,9 @@ function view() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<QuestionArticle
-				uuid="34fb503b"
-				type="Multiple Choice"
-				options={{ trueOptions: 3, falseOptions: 4 }}
-				content="The adrenal medulla secretes which of the following in the greatest quantity?"
-				tags={["Parasitology", "Chemistry", "Biology"]}
-			/>
+			<div className="page column">
+				<ArticleContainer questions={questionSet} />
+			</div>
 		</div>
 	);
 }
