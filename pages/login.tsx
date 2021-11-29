157da8603e99aef3470dc265e291f5799de52648
rxrosli <raymark.roslin@gmail.com>
2021-11-29 09:30:23 +0800

    feat: create login page

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function login() {
+	return (
+		<>
+			{/* <body className="auth-body" />
+			<div className="auth-container">
+				<h1 className="auth-form-header">Sign in to qBank</h1>
+				<div></div>
+			</div> */}
+		</>
+	);
+}
