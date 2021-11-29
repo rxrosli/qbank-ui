3b54fc01119a9fe1bd98c6bdc104ae3acf35c808
rxrosli <raymark.roslin@gmail.com>
2021-11-29 11:31:38 +0800

    feat: add inputs in authentication

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,11 +3,17 @@ import React from "react";
 export default function login() {
 	return (
 		<>
-			{/* <body className="auth-body" />
+			<body className="auth-body" />
 			<div className="auth-container">
 				<h1 className="auth-form-header">Sign in to qBank</h1>
-				<div></div>
-			</div> */}
+				<div className="auth-form-body">
+					<label className="auth-label">Username or email address</label>
+					<input className="auth-input" type="text" />
+					<label className="auth-label">Password</label>
+					<input className="auth-input" type="password" />
+					<button className="auth-submit" children="Sign In" />
+				</div>
+			</div>
 		</>
 	);
 }
