391d49085f78137d62fde017b2ad54b94feda303
rxrosli <raymark.roslin@gmail.com>
2022-02-08 15:40:18 +0800

    feat: create exam model

---
+++ models/IExam.ts
@@ -0,0 +1,10 @@
+import IQuestion from "./IQuestion";
+
+export default interface IExam {
+	_id?: string;
+	title: string;
+	description: string;
+	author: string;
+	settings: { [key: string]: string };
+	questions: IQuestion[];
+}
