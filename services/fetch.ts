d7f1504929dac3c7b0689adc4ba50c4ecfaca6cb
rxrosli <raymark.roslin@gmail.com>
2022-02-15 13:38:11 +0800

    refactor: make body property optional, simplified fetchApi function

--- services/fetch.ts
+++ services/fetch.ts
@@ -1,69 +1,66 @@
+import { useEffect } from "react";
 import axios, { Method } from "axios";
+import Router from "next/router";
 
+export type FetchApiBody = { [key: string]: any };
+export type FetchApiParams = {
+	uri: string;
+	method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+	body?: FetchApiBody;
+};
 export type FetchApiEvents = {
 	onLoad?: () => Promise<void>;
 	onSuccess?: (data: any) => Promise<void>;
 	onError?: (error: any) => Promise<void>;
 	onTokenExpired?: () => Promise<void>;
 };
-
-export type FetchApiBody = { [key: string]: any }; // string | number | null | undefined
-
-export type FetchApiParams = { uri: string } & {
-	method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-	body: FetchApiBody;
-};
 export async function fetchApi(params: FetchApiParams, fetchApiEvents: FetchApiEvents) {
-	const { uri, method, body } = params;
-	const { onLoad, onSuccess, onError, onTokenExpired } = fetchApiEvents;
-
+	const { uri, method, body = {} } = params;
+	const {
+		onLoad = async () => {},
+		onSuccess = async () => {},
+		onError = async () => {},
+		onTokenExpired = refreshToken
+	} = fetchApiEvents;
+	const onSuccessHandler = async () => {
+		await onSuccess(await axiosHandler(uri, method, body));
+	};
 	try {
-		if (typeof onLoad === "function") await onLoad();
-		const response = await axiosHandler(uri, method, body);
-		if (onSuccess) await onSuccess(response);
+		await onLoad();
+		await onSuccessHandler();
 	} catch (error) {
-		console.log(JSON.stringify(error));
-		if (error.response !== undefined) {
-			const message = error.response.data.error.message;
-			if (onTokenExpired && message.name === "TokenExpiredError") {
-				console.log(message.name);
-				try {
-					await onTokenExpired();
-					const response = await axiosHandler(uri, method, body);
-					if (onSuccess) await onSuccess(response);
-				} catch (error) {
-					if (onError) await onError(error);
-				}
-			} else {
-				if (onError) await onError(error);
-			}
-		} else {
-			if (onError) await onError(error);
+		if (
+			error.response === undefined ||
+			error.response.data.error.message.name !== "TokenExpiredError"
+		)
+			await onError(error);
+		try {
+			await onTokenExpired();
+			await onSuccessHandler();
+		} catch (error) {
+			await onError(error);
 		}
 	}
 }
-
 async function axiosHandler(uri: string, method: Method, body: object) {
-	const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-	const authorization = "bearer " + window.localStorage.getItem("token");
 	return await axios({
-		baseURL: baseUrl,
+		baseURL: process.env.NEXT_PUBLIC_API_URL,
 		url: uri,
 		method: method,
 		headers: {
-			Authorization: authorization,
+			Authorization: "bearer " + window.localStorage.getItem("token"),
 			"Content-Type": "application/json"
 		},
 		data: body
 	});
 }
-export function authenticated() {
+export function authenticated(): boolean {
+	const username = window.localStorage.getItem("username");
 	const token = window.localStorage.getItem("token");
 	const refresh = window.localStorage.getItem("refreshToken");
-	if (!token && !refresh) return false;
+	if (!username && !token && !refresh) return false;
 	return true;
 }
-
 export async function refreshToken() {
 	const auth = process.env.NEXT_PUBLIC_API_URL + "/auth";
 	const refresh = window.localStorage.getItem("refreshToken");
@@ -81,4 +78,13 @@ export async function refreshToken() {
 		.catch(err => {
 			console.log(err);
 		});
+}
+export function onPageLoad(onSuccess: () => void) {
+	useEffect(() => {
+		if (!authenticated()) {
+			Router.push("/login");
+			return;
+		}
+		onSuccess();
+	});
 }
