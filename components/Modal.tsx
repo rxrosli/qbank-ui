31fd2faaedbe2efa447b6400e7d3148a5eb292c3
rxrosli <raymark.roslin@gmail.com>
2022-02-23 02:07:23 +0800

    feat: create modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,18 +4,19 @@ import Icon from "./Icon";
 type Props = {
 	title?: string;
 	isActive: boolean;
+	width?: "xs" | "s";
 	onCloseClick?: React.MouseEventHandler<HTMLDivElement>;
 	children?: React.ReactNode;
 };
 
 const Modal = (props: Props) => {
-	const { title, isActive, onCloseClick, children } = props;
+	const { title, isActive, width, onCloseClick, children } = props;
 	const modalBackdropclassName = isActive
 		? "modal__backdrop modal__backdrop--active"
 		: "modal__backdrop";
 	return (
 		<div className={modalBackdropclassName}>
-			<div className="modal">
+			<div className={width ? `modal modal--width-${width}` : "modal"}>
 				<div className="section">
 					<div className="modal__title">{title}</div>
 					<Icon type="close_small" className="modal__close" onClick={onCloseClick}></Icon>
